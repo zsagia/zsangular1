5fd25c069ee32938ed93913bc5a7f235e4f0737b
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-10-18 09:46:37 +0200

    Part 1 of Hero App

--- app/app.component.js
+++ app/app.component.js
@@ -8,14 +8,24 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
+var core_1 = require("@angular/core");
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
 var AppComponent = (function () {
     function AppComponent() {
+        this.hero = {
+            id: 1,
+            name: 'Windstorm'
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular Application example</h1>'
+            template: "\n        <h1>{{title}}</h1>\n        <h2>{{hero.name}} details!</h2>\n        <div><label>id: </label>{{hero.id}}</div>\n        <div>\n            <label>name: </label>\n            <input [(ngModel)] = \"hero.name\" placeholder=\"name\">\n        </div>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
