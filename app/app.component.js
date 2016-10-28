5bc2ff7816a1609ee183058d57f22fad5aee632e
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-10-28 22:01:57 +0200

    Regenerated

--- app/app.component.js
+++ app/app.component.js
@@ -8,24 +8,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require("@angular/core");
-var Hero = (function () {
-    function Hero() {
-    }
-    return Hero;
-}());
-exports.Hero = Hero;
+var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
-        this.hero = {
-            id: 1,
-            name: 'Windstorm'
-        };
+        this.title = 'Tour of Heroes';
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n        <h1>{{title}}</h1>\n        <h2>{{hero.name}} details!</h2>\n        <div><label>id: </label>{{hero.id}}</div>\n        <div>\n            <label>name: </label>\n            <input [(ngModel)] = \"hero.name\" placeholder=\"name\">\n        </div>\n    "
+            template: "\n        <h1>{{title}}</h1>\n        <my-heroes>\n     "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.module.js
+++ app/app.module.js
@@ -11,7 +11,10 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var forms_1 = require('@angular/forms');
+var hero_detail_component_1 = require('./hero-detail.component');
 var app_component_1 = require('./app.component');
+var heroes_component_1 = require('./heroes.component');
+var hero_service_1 = require('./hero.service');
 var AppModule = (function () {
     function AppModule() {
     }
@@ -19,8 +22,16 @@ var AppModule = (function () {
         core_1.NgModule({
             imports: [
                 platform_browser_1.BrowserModule,
-                forms_1.FormsModule],
-            declarations: [app_component_1.AppComponent],
+                forms_1.FormsModule
+            ],
+            declarations: [
+                app_component_1.AppComponent,
+                heroes_component_1.HeroesComponent,
+                hero_detail_component_1.HeroDetailComponent
+            ],
+            providers: [
+                hero_service_1.HeroService
+            ],
             bootstrap: [app_component_1.AppComponent]
         }), 
         __metadata('design:paramtypes', [])
