b90bb5f3fcb345b9e30ab96714a1d40d56960f93
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-10-28 22:01:39 +0200

    Modify component and module classes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,14 @@
-import {Component} from "@angular/core";
-
-export class Hero {
-    id: number;
-    name: string;
-}
+import { Component } from '@angular/core';
+import { HeroesComponent } from 'hero.component';
 
 @Component({
-    selector: 'my-app',
-    template: `
+     selector: 'my-app',
+     template: `
         <h1>{{title}}</h1>
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div>
-            <label>name: </label>
-            <input [(ngModel)] = "hero.name" placeholder="name">
-        </div>
-    `
+        <my-heroes>
+     `
 })
 
 export class AppComponent {
-    title: 'Tour of Heroes';
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+     title = 'Tour of Heroes';
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail.component';
 
 import { AppComponent }   from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [ 
     BrowserModule,
-    FormsModule ],
-  declarations: [ AppComponent ],
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent
+  ],
+  providers: [
+    HeroService
+  ],
   bootstrap:    [ AppComponent ]
 })
 
