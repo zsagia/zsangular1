be1929f2642807b2ac0001bb3a8a1bc198c7e863
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-10-31 16:07:05 +0100

    Refactoring hero-detail component with moving template to a different file

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -6,18 +6,10 @@ import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
-  selector: 'my-hero-detail',
-  template: `
-    <div *ngIf="hero">
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-        </div>
-        <button (click)="goBack()">Back</button>
-    </div>
-    `
+    moduleId: module.id,
+    selector: 'my-hero-detail',
+    templateUrl: 'hero-detail.component.html',
+    styleUrls: ['hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit {
