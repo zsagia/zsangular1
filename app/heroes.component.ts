2bdb70b64db38856982af1bb1c12950087361987
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-10-31 11:43:52 +0100

    Go back using getHeroes service with a normal speed

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -25,7 +25,7 @@ export class HeroesComponent implements OnInit {
     }
 
     getHeroes(): void {
-        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
     }
 
     ngOnInit(): void {
