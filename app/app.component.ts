a6c17264cc0fe0a5dca4de99b03db58583772954
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-10-31 16:10:13 +0100

    Refactoring app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { HeroesComponent } from './heroes.component';
 
 @Component({
-     selector: 'my-app',
-     template: `
+    moduleId: module.id,
+    selector: 'my-app',
+    template: `
         <h1>{{title}}</h1>
         <nav>
             <a routerLink="/dashboard">Dashboard</a>
             <a routerLink="/heroes">Heroes</a>
         </nav>
         <router-outlet></router-outlet>
-     `
+    `,
+    styleUrls: ['app.component.css']
 })
 
 export class AppComponent {
